fdc2cd76049b13a731df52ee750c1bcc175fe9fa
Ernesto Auerbach <auerbachrea@gmail.com>
2021-12-02 08:32:03 -0600

    -Module 5 Lesson 4 & 5 -Added & coded HomeView -Added & coded HomeViewRow -Added & coded ContentModel -Added & coded ContentView -Added & coded ContentViewRow

---
+++ LearningApp/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by AM Rust - Ernesto Auerbach  on 12/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            LazyVStack {
+                
+                // Confirm that currentModule is set
+                if model.currentModule != nil {
+                
+                    ForEach(0..<model.currentModule!.content.lessons.count) { index in
+                        
+                        ContentViewRow(index: index)
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
+            
+        }
+        
+        
+    }
+}
+
