04a05a25cfbc622d545f14b3fcf8c7214b31a863
Ernesto Auerbach <auerbachrea@gmail.com>
2021-12-08 08:51:23 -0600

    -AVKit -Edit HomeView -Edit ContentModel -Add & code TestView -Add & code CodeTextView -Add & code RectangleCard

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by AM Rust - Ernesto Auerbach  on 12/2/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+    
+}
